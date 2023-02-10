2ca62b5fae1041cf4ffdc93a6e8de3b27d65bdaf
Jeremy <jeremy.chenet@epitech.eu>
2023-02-10 03:15:09 +0100

    [+] proxy/user mode && secret mode with actual delay in case of waiting

--- client/main.cpp
+++ client/main.cpp
@@ -1,52 +1,186 @@
 #include <iostream>
 #include <string>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <thread>
+#include <chrono>
 
 using boost::asio::ip::tcp;
 
 class TcpClient
 {
 public:
-    TcpClient(boost::asio::io_service& io_service)
-        : socket_(io_service)
+    TcpClient(boost::asio::io_context& io_context, bool secret_mode, std::string secret) : _socket(io_context), _timer(io_context), _secret_mode(secret_mode), _secret(secret)
+    {}
+
+    ~TcpClient() {
+        _socket.close();
+        std::cout << "Disconnected from server." << std::endl;
+    }
+
+    void start(tcp::resolver::results_type endpoints)
     {
-        boost::asio::ip::tcp::endpoint endpoint(boost::asio::ip::address::from_string("127.0.0.1"), 12345);
-        socket_.connect(endpoint);
-        std::cout << "Connected to server" << std::endl;
+        _endpoints = endpoints;
+        start_connect(endpoints.begin());
+    }
+    void start_connect(tcp::resolver::results_type::iterator endpoint_iter)
+    {
+        if (endpoint_iter != _endpoints.end())
+        {
+            std::cout << "Trying to connect to " << endpoint_iter->endpoint() << "...\n";
+
+            // Start the asynchronous connect operation.
+            _socket.async_connect(endpoint_iter->endpoint(),
+                                  boost::bind(&TcpClient::handle_connect, this,
+                                              boost::placeholders::_1, endpoint_iter));
+        }
+    }
+
+    void handle_connect(const boost::system::error_code& ec,
+                        tcp::resolver::results_type::iterator endpoint_iter)
+    {
+        if (ec) {
+            std::cout << "Connect error: " << ec.message() << "\n";
+            _socket.close();
+            start_connect(++endpoint_iter);
+        } else {
+            std::cout << "Connected to " << endpoint_iter->endpoint() << "\n";
+            start_read();
+            if (_secret_mode) {
+                sendMessage("[CMD]SETSECRET " + _secret);
+            }
+            _timer.expires_from_now(std::chrono::seconds(7));
+            _timer.async_wait(boost::bind(&TcpClient::handle_timer, this, _1));
+        }
     }
 
     void sendMessage(const std::string& message)
     {
-        boost::asio::write(socket_, boost::asio::buffer(message));
-        std::cout << "Message sent: " << message << std::endl;
+        boost::asio::async_write(_socket, boost::asio::buffer(message), [this](boost::system::error_code ec, std::size_t length)
+        {
+            if (ec) {
+                std::cout << "Error: " << ec.message() << std::endl;
+            }
+        });
     }
 
 private:
-    tcp::socket socket_;
-};
-
-int main()
-{
-    try
+    void start_read()
     {
-        boost::asio::io_service io_service;
-        TcpClient client(io_service);
+        _socket.async_read_some(boost::asio::buffer(_data, MAX_LENGTH),
+                                boost::bind(&TcpClient::handle_read, this,
+                                            boost::asio::placeholders::error,
+                                            boost::asio::placeholders::bytes_transferred));
+    }
 
-        std::string message;
-        while (true)
-        {
-            std::cout << "> ";
-            std::getline(std::cin, message);
-            if (message == "exit")
-                break;
-            client.sendMessage(message);
+    void handle_command(std::string message) {
+
+        // if message starts with "[CMD]ECHOREPLY "
+        if (message.find("[CMD]ECHOREPLY ") == 0) {
+            std::cout << "Received: " << message << std::endl;
+            sendMessage(message.substr(std::string("[CMD]ECHOREPLY ").length()));
+        } else {
+            std::cout << "Received: " << message << std::endl;
         }
     }
-    catch (std::exception& e)
+
+    void handle_read(const boost::system::error_code& ec, std::size_t n)
     {
+        if (!ec) {
+            if (_other_client_connected) {
+                handle_command(std::string(_data));
+            }
+            if (!_other_client_connected && std::string(_data).find("OK 1") != std::string::npos) {
+                std::cout << "Waiting for the other client to connect..." << std::endl;
+            }
+            if (!_other_client_connected && !_secret_mode && std::string(_data).find("OK 2") != std::string::npos) {
+                _timer.cancel();
+                std::cout << "Other client is now connected." << std::endl;
+                _other_client_connected = true;
+            }
+            if (!_other_client_connected && _secret_mode && std::string(_data).find("OK SECRET") != std::string::npos) {
+                _timer.cancel();
+                std::cout << "Other client with the same secret is now connected." << std::endl;
+                _other_client_connected = true;
+            }
+            memset(_data, 0, MAX_LENGTH);
+            start_read();
+        } else {
+            std::cout << "Server disconnected." << std::endl;
+        }
+    }
+
+    void handle_timer(const boost::system::error_code &error)
+    {
+        if (!error)
+        {
+            if(!_secret_mode)
+                std::cout << "No other client has connected after 7 seconds. Closing the program." << std::endl;
+            else
+                std::cout << "No other client with the same password has connected after 7 seconds. Closing the program." << std::endl;
+            _socket.close();
+            exit(84);
+        }
+    }
+
+    tcp::socket _socket;
+    std::thread _reading_thread;
+    tcp::resolver::results_type _endpoints;
+    std::string _input_buffer;
+    boost::asio::steady_timer _timer;
+    std::string _secret = "";
+
+    enum { MAX_LENGTH = 1024 };
+    char _data[MAX_LENGTH] = {0};
+    bool _other_client_connected = false;
+    bool _secret_mode = false;
+};
+
+void run_client_session(int ac, char **av)
+{
+    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::resolver resolver(io_context);
+    bool secret_mode = false;
+    if (ac == 4)
+        secret_mode = true;
+    std::string secret = "";
+    if (secret_mode)
+        secret = std::string(av[3]);
+    TcpClient client(io_context, secret_mode, secret);
+
+    // Start the client session.
+    client.start(resolver.resolve(av[1], av[2]));
+
+    //thread to run the io_context.
+    std::thread t([&io_context](){ io_context.run(); });
+
+    // Starting prompt here.
+    usleep(100000);
+    std::string message;
+    while (true)
+    {
+        std::cout << "> ";
+        std::getline(std::cin, message);
+        if (message == "exit")
+            break;
+        client.sendMessage(message);
+    }
+    if (t.joinable())
+        t.join();
+}
+
+int main(int ac, char **av)
+{
+    try {
+        if (ac == 2 && av[1] == std::string("-h")) {
+            std::cout << "Usage: ./client [host] [port] [secret]" << std::endl;
+            return 0;
+        }
+        run_client_session(ac, av);
+
+    } catch (std::exception& e) {
         std::cerr << "Exception: " << e.what() << std::endl;
     }
 
     return 0;
 }
-
--- server/main.cpp
+++ server/main.cpp
@@ -1,95 +1,181 @@
 #include <iostream>
-#include <string>
 #include <boost/asio.hpp>
+#include <array>
 
+using namespace std;
 using boost::asio::ip::tcp;
 
-class TcpConnection : public std::enable_shared_from_this<TcpConnection>
+class TCPServer
 {
 public:
-    typedef std::shared_ptr<TcpConnection> pointer;
-
-    static pointer create(boost::asio::io_context& io_context)
+    TCPServer(boost::asio::io_context& io_context, int port, bool secret_mode)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port)), _secret_mode(secret_mode)
     {
-        return pointer(new TcpConnection(io_context));
+        start_accept();
     }
 
-    tcp::socket& socket()
+    ~TCPServer()
     {
-        return socket_;
+        for (auto socket : _sockets)
+            delete std::get<SOCKET>(socket);
     }
 
-    void start()
-    {
-        boost::asio::async_read(socket_, boost::asio::buffer(data_),
-            [this, self = shared_from_this()](boost::system::error_code ec, std::size_t length)
-            {
-                if (!ec)
-                {
-                    std::cout << "Received message: ";
-                    std::cout.write(data_.data(), length);
-                    std::cout << std::endl;
-                }
-                else
-                {
-                    std::cerr << "Error: " << ec.message() << std::endl;
-                }
-            });
-    }
 
 private:
-    TcpConnection(boost::asio::io_context& io_context)
-        : socket_(io_context)
-    {
-    }
 
-    tcp::socket socket_;
-    std::array<char, 1024> data_;
-};
-
-class TcpServer
-{
-public:
-    TcpServer(boost::asio::io_context& io_context)
-        : acceptor_(io_context, tcp::endpoint(tcp::v4(), 12345))
+    void send_to_all_clients(const std::string& message)
     {
-        startAccept();
-    }
-
-private:
-    void startAccept() 
-    {
-        TcpConnection::pointer newConnection = TcpConnection::create(acceptor_.get_executor().get_context());
-        acceptor_.async_accept(newConnection->socket(),
-            [this, newConnection](boost::system::error_code ec)
+        for (auto socket : _sockets) {
+            boost::asio::async_write(*std::get<SOCKET>(socket), boost::asio::buffer(message), [this](boost::system::error_code ec, std::size_t length)
             {
-                if (!ec)
-                {
-                    newConnection->start();
-                    std::cout << "Accepted connection from " << newConnection->socket().remote_endpoint().address().to_string() << std::endl;
+                if (ec) {
+                    std::cout << "Error: " << ec.message() << std::endl;
                 }
-                else
-                {
-                    std::cerr << "Error: " << ec.message() << std::endl;
-                }
-                startAccept();
             });
+        }
     }
+    void start_accept()
+    {
+        // Create a new potential socket:
+        tcp::socket *socket = new tcp::socket(acceptor_.get_executor());
+        acceptor_.async_accept(*socket, [this, socket](boost::system::error_code ec)
+        {
+            if (!ec) {
+                std::tuple<tcp::socket*, std::string> new_client = std::make_tuple(socket, "");
+                std::cout << "New client connected." << std::endl;
+                _sockets.push_back(new_client);
+                _nb_clients += 1;
+                start_read(*socket);
+            }
+            else {
+                delete socket;
+            }
+            // Get ready for a new client connection until we have 2 clients:
+            if (_nb_clients < 2) {
+                send_to_all_clients("OK 1");;
+            }
+            else {
+                std::cout << "Both clients are connected." << std::endl;
+                send_to_all_clients("OK 2");
+            }
+            start_accept();
+        });
+    }
+
+    void start_read(tcp::socket& socket)
+    {
+        socket.async_read_some(boost::asio::buffer(_data, MAX_LENGTH), [this, &socket](boost::system::error_code ec, std::size_t length)
+        {
+            if (!ec) {
+                if (strncmp(_data, "[CMD]SETSECRET ", LENGTH_SECRET_CMD) == 0) {
+                    std:string secret = std::string(_data).substr(std::string("[CMD]SETSECRET ").length());
+                    set_secret_of_the_client(socket, secret);
+                    find_client_with_same_secret(socket, secret);
+                } else {
+                    std::cout << "Data received: " << std::string(_data, length) << std::endl;
+                    send_message_to_the_other_client(socket, std::string(_data, length));
+                    memset(_data, 0, MAX_LENGTH);
+                }
+                start_read(socket);
+            } else {
+                if (ec.message() == "End of file")
+                    std::cout << "Client disconnected." << std::endl;
+                else
+                    std::cout << "Error: " << ec.message() << std::endl;
+                for (auto it = _sockets.begin(); it != _sockets.end(); ++it) {
+                    if (std::get<SOCKET>(*it) == &socket) {
+                        _sockets.erase(it);
+                        break;
+                    }
+                }
+                _nb_clients -= 1;
+            }
+        });
+    }
+
+    void send_message_to_the_other_client(tcp::socket& socket, const std::string& message)
+    {
+        int i = 0;
+        for (auto s : _sockets) {
+            if (std::get<SOCKET>(s) != &socket) {
+                boost::asio::async_write(*std::get<SOCKET>(s), boost::asio::buffer(message), [this](boost::system::error_code ec, std::size_t length)
+                {
+                    if (ec) {
+                        std::cout << "Error: " << ec.message() << std::endl;
+                    }
+                });
+            }
+        }
+    }
+
+    void send_message_to_a_specific_client(tcp::socket& socket, const std::string& message)
+    {
+        boost::asio::async_write(socket, boost::asio::buffer(message), [this](boost::system::error_code ec, std::size_t length)
+        {
+            if (ec) {
+                std::cout << "Error: " << ec.message() << std::endl;
+            }
+        });
+    }
+
+    void set_secret_of_the_client(tcp::socket& socket, const std::string& secret)
+    {
+        if (!_secret_mode)
+            return;
+        for (auto it = _sockets.begin(); it != _sockets.end(); ++it) {
+            if (std::get<SOCKET>(*it) == &socket) {
+                std::get<SECRET>(*it) = secret;
+                break;
+            }
+        }
+    }
+
+    void find_client_with_same_secret(tcp::socket& socket, const std::string& secret)
+    {
+        for (auto s : _sockets) {
+            if (std::get<SOCKET>(s) != &socket && std::get<SECRET>(s) == secret) {
+                    send_message_to_a_specific_client(socket, "OK SECRET");
+                    send_message_to_a_specific_client(*std::get<SOCKET>(s), "OK SECRET");
+                }
+            }
+    }
+
 
     tcp::acceptor acceptor_;
+    std::vector<std::tuple<tcp::socket*, std::string>> _sockets;
+
+    bool _secret_mode = false;
+    enum { 
+        SOCKET = 0,
+        SECRET = 1,
+        LENGTH_SECRET_CMD = 15,
+        MAX_LENGTH = 1024,
+    };
+    char _data[MAX_LENGTH] = {0};
+    short _nb_clients = 0;
 };
 
-int main()
+void run_server_session(int port, bool secret_mode = false)
 {
-    try
-    {
-        boost::asio::io_context io_context;
-        auto& executor = io_context.get_executor().context();
-        TcpServer server(io_context);
-        io_context.run();
+    boost::asio::io_context io_context;
+    TCPServer server(io_context, port, secret_mode);
+    std::cout << "Server started, waiting for connection..." << std::endl;
+    io_context.run();
+}
+
+int main(int ac, char* av[])
+{
+    try {
+        if ((ac == 2 && std::string(av[1]) == "-h") || (ac < 2 || ac > 3)) {
+            cerr << "Usage: TCPServer <port> -secret" << endl;
+            return 0;
+        }
+        bool secret_mode = false;
+        if (ac == 3 && av[2] == std::string("--secret"))
+            secret_mode = true;
+        run_server_session(atoi(av[1]), secret_mode);
+    } catch (exception& e) {
+        cerr << "Exception: " << e.what() << endl;
     }
-    catch (std::exception& e)
-    {
-        std::cerr << "Exception: " << e.what() << std::endl;
-    }
+    return 0;
 }
